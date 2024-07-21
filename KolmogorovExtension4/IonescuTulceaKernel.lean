5da1c5f7b68c2e95b835feef7b89e82d2fd39d2d
Etienne Marion <etienne.marion@ens-lyon.fr>
2024-07-21 11:36:43 +0200

    Change file names

--- KolmogorovExtension4/IonescuTulceaKernel.lean
+++ KolmogorovExtension4/IonescuTulceaKernel.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Etienne Marion. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Etienne Marion
 -/
-import KolmogorovExtension4.meilleure_composition
+import KolmogorovExtension4.compProdNat
 import KolmogorovExtension4.Projective
 import KolmogorovExtension4.DependsOn
 import Mathlib.MeasureTheory.Measure.ProbabilityMeasure
--- KolmogorovExtension4/ProductMeasure.lean
+++ KolmogorovExtension4/ProductMeasure.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Etienne Marion. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Etienne Marion
 -/
-import KolmogorovExtension4.IonescuTulceaFinset3
+import KolmogorovExtension4.IonescuTulceaKernel
 
 open MeasureTheory MeasurableSpace ProbabilityTheory Finset ENNReal Filter Topology Function kernel
 
