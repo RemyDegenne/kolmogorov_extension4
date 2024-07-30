e30505b819687b084fef076d88785818b474c97f
RemyDegenne <Remydegenne@gmail.com>
2024-07-30 10:13:13 +0200

    add PR number

--- KolmogorovExtension4/AuxLemmas.lean
+++ KolmogorovExtension4/AuxLemmas.lean
@@ -10,29 +10,27 @@ open Finset Set Filter
 
 open scoped ENNReal NNReal Topology
 
+-- PR #15294
 lemma Finset.sUnion_disjiUnion {α β : Type*} {f : α → Finset (Set β)} (I : Finset α)
     (hf : (I : Set α).PairwiseDisjoint f) :
     ⋃₀ (I.disjiUnion f hf : Set (Set β)) = ⋃ a ∈ I, ⋃₀ ↑(f a) := by
   ext
   simp only [coe_disjiUnion, mem_coe, Set.mem_sUnion, Set.mem_iUnion, exists_prop]
-  constructor
-  · rintro ⟨t, ⟨a, haI, hatf⟩, hbt⟩
-    exact ⟨a, haI, t, hatf, hbt⟩
-  · rintro ⟨a, haI, t, hatf, hbt⟩
-    exact ⟨t, ⟨a, haI, hatf⟩, hbt⟩
+  tauto
 
 lemma Finset.sum_image_le_of_nonneg {ι α β : Type*} [DecidableEq α]
     [OrderedAddCommMonoid β] [SMulPosMono ℕ β]
     {J : Finset ι} {g : ι → α} {f : α → β} (hf : ∀ u ∈ J.image g, 0 ≤ f u) :
     ∑ u in J.image g, f u ≤ ∑ u in J, f (g u) := by
   rw [sum_comp f g]
-  refine sum_le_sum fun a hag => ?_
+  refine sum_le_sum fun a hag ↦ ?_
   obtain ⟨i, hi, hig⟩ := Finset.mem_image.mp hag
-  conv_lhs => rw [← one_smul ℕ (f a)]
+  conv_lhs => rw [← one_nsmul (f a)]
   refine smul_le_smul_of_nonneg_right ?_ (hf a hag)
   rw [Nat.one_le_iff_ne_zero, ← Nat.pos_iff_ne_zero, card_pos]
   exact ⟨i, mem_filter.mpr ⟨hi, hig⟩⟩
 
+-- PR #15294
 @[to_additive]
 lemma Finset.prod_image_of_disjoint {α β : Type*} [PartialOrder α] [OrderBot α] [DecidableEq α]
     [CommMonoid β] {g : α → β}
