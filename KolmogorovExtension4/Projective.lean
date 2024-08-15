9a795ef066632040148ef8b829fdea839955208e
Etienne Marion <etienne.marion@ens-lyon.fr>
2024-08-15 11:56:14 +0200

    Rewrite Projective

--- KolmogorovExtension4/Projective.lean
+++ KolmogorovExtension4/Projective.lean
@@ -75,9 +75,8 @@ theorem IsProjectiveMeasureFamily.congr_cylinder
     P I S = P J T := by
   classical
   let U :=
-    (fun f : ∀ i : (I ∪ J : Finset ι), α i
-        ↦ fun j : I ↦ f ⟨j, Finset.mem_union_left J j.prop⟩) ⁻¹' S ∩
-      (fun f ↦ fun j : J ↦ f ⟨j, Finset.mem_union_right I j.prop⟩) ⁻¹' T
+    (projSubset' Finset.subset_union_left) ⁻¹' S ∩
+      (projSubset' Finset.subset_union_right) ⁻¹' T
   suffices P (I ∪ J) U = P I S ∧ P (I ∪ J) U = P J T from this.1.symm.trans this.2
   constructor
   · have h_eq_union : cylinder I S = cylinder (I ∪ J) U := by
@@ -90,9 +89,8 @@ theorem IsProjectiveMeasureFamily.congr_cylinder
 theorem IsProjectiveMeasureFamily.measure_univ_eq_of_subset (hP : IsProjectiveMeasureFamily P)
     (I J : Finset ι) (hJI : J ⊆ I) : P I univ = P J univ := by
   classical
-  have : (univ : Set (∀ i : I, α i)) =
-      (fun x : ∀ i : I, α i ↦ fun i : J ↦ x ⟨i, hJI i.2⟩) ⁻¹' (univ : Set (∀ i : J, α i)) :=
-    by rw [preimage_univ]
+  have : (univ : Set (∀ i : I, α i)) = (projSubset' hJI) ⁻¹' univ := by
+    rw [preimage_univ]
   rw [this, ← Measure.map_apply _ MeasurableSet.univ]
   · rw [hP I J hJI]
   · exact measurable_projSubset' hJI
