64f76b09459e9f2d8b687f8e5d27570b7ee9324f
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2025-01-30 10:20:36 +0100

    restore IsSetField, close namespaces

--- KolmogorovExtension4/Semiring.lean
+++ KolmogorovExtension4/Semiring.lean
@@ -5,7 +5,6 @@ Authors: Rémy Degenne, Peter Pfaffelhuber
 -/
 import KolmogorovExtension4.AuxLemmas
 import Mathlib.MeasureTheory.SetSemiring
-import KolmogorovExtension4.Ordered
 
 /-! # Semirings of sets
 
@@ -75,3 +74,41 @@ lemma sUnion_unionDisjointOfUnion (hC : IsSetSemiring C) (hJ : ↑J ⊆ C) :
   tauto
 
 end AllDiffFinset₀
+
+end IsSetSemiring
+
+/-- A field of sets is a family of sets which is stable under union, difference, and contains
+the empty set and the whole space. -/
+structure IsSetField (C : Set (Set α)) extends IsSetRing C : Prop where
+  univ_mem : Set.univ ∈ C
+
+namespace IsSetField
+
+theorem inter_mem (hC : IsSetField C) (hs : s ∈ C) (ht : t ∈ C) : s ∩ t ∈ C :=
+  hC.toIsSetRing.inter_mem hs ht
+
+theorem compl_mem (hC :IsSetField C) (hs : s ∈ C) : sᶜ ∈ C := by
+  rw [compl_eq_univ_diff]; exact hC.diff_mem hC.univ_mem hs
+
+theorem toIsSetSemiring (hC :IsSetField C) : IsSetSemiring C :=
+  hC.toIsSetRing.isSetSemiring
+
+theorem iUnion_le_mem (hC :IsSetField C) {s : ℕ → Set α} (hs : ∀ n, s n ∈ C) (n : ℕ) :
+    (⋃ i ≤ n, s i) ∈ C :=
+  hC.toIsSetRing.iUnion_le_mem hs n
+
+theorem iInter_le_mem (hC :IsSetField C) {s : ℕ → Set α} (hs : ∀ n, s n ∈ C) (n : ℕ) :
+    (⋂ i ≤ n, s i) ∈ C :=
+  hC.toIsSetRing.iInter_le_mem hs n
+
+theorem partialSups_mem (hC :IsSetField C) {s : ℕ → Set α} (hs : ∀ n, s n ∈ C) (n : ℕ) :
+    partialSups s n ∈ C :=
+  hC.toIsSetRing.partialSups_mem hs n
+
+theorem disjointed_mem (hC :IsSetField C) {s : ℕ → Set α} (hs : ∀ n, s n ∈ C) (n : ℕ) :
+    disjointed s n ∈ C :=
+  hC.toIsSetRing.disjointed_mem hs n
+
+end IsSetField
+
+end MeasureTheory
