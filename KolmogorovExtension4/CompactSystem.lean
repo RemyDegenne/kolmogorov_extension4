aa8b540db02c68bb7ebdf2033d5d7934df7b7d0b
Remy Degenne <remydegenne@gmail.com>
2025-03-11 20:07:48 +0100

    bump Mathlib: delete Boxes.lean

--- KolmogorovExtension4/CompactSystem.lean
+++ KolmogorovExtension4/CompactSystem.lean
@@ -3,10 +3,10 @@ Copyright (c) 2023 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne, Peter Pfaffelhuber
 -/
-import KolmogorovExtension4.Boxes
 import KolmogorovExtension4.ClosedProj
+import Mathlib.MeasureTheory.Constructions.ClosedCompactCylinders
 
-open Set
+open Set MeasureTheory
 
 section definition
 
